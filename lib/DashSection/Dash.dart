7788e37e3fc10f63cbe201bb9433546ff5e580d9
TiagoMLucio <86849293+TiagoMLucio@users.noreply.github.com>
2021-07-14 20:09:56 -0300

    Add files via upload

--- lib/DashSection/Dash.dart
+++ lib/DashSection/Dash.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 
 import 'Constantes.dart';
