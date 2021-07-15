c6a11e8a0c0a7895910963e875b608ce749b95ff
Tiago Lucio <tiagolucio100@gmail.com>
2021-07-15 01:06:29 -0300

    monitoramento integrado com chat

---
+++ lib/DashSection/cardMensagens.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+import 'Modelos/modTarefas.dart';
+import 'Constantes.dart';
+
+class TarefaInfoCard extends StatelessWidget {
+  const TarefaInfoCard({
+    Key? key,
+    required this.info,
+  }) : super(key: key);
+
+  final Tarefa info;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(defaultPadding),
+      decoration: BoxDecoration(
+        color: secondaryColor,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            info.nome!,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "${listatarefas.length} Tarefas",
+                style: Theme.of(context)
+                    .textTheme
+                    .caption!
+                    .copyWith(color: Colors.black),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProgressLine extends StatelessWidget {
+  const ProgressLine({
+    Key? key,
+    this.color = primaryColor,
+    required this.percentage,
+  }) : super(key: key);
+
+  final Color? color;
+  final int? percentage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 5,
+          decoration: BoxDecoration(
+            color: color!.withOpacity(0.1),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+        ),
+        LayoutBuilder(
+          builder: (context, constraints) => Container(
+            width: constraints.maxWidth * (percentage! / 100),
+            height: 5,
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
